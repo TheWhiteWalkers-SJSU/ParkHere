427d76d7a6b1a9e9599ec8b7552b6d292ed5ff36
Rohit Mathew <rohit@rabb.it>
2017-10-28 00:45:26 -0700

    Display user's listings on the homepage

---
+++ app/src/main/java/com/thewhitewalkers/parkhere/ListingList.java
@@ -0,0 +1,45 @@
+package com.thewhitewalkers.parkhere;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Rohit on 10/27/17.
+ */
+
+public class ListingList extends ArrayAdapter<Listing> {
+
+    private Activity context;
+    private List<Listing> listingList;
+
+    public ListingList(Activity context, List<Listing> listingList){
+        super(context, R.layout.homepage_listings, listingList);
+        this.context = context;
+        this.listingList = listingList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.homepage_listings, null, true);
+
+        TextView textViewName = listViewItem.findViewById(R.id.textViewListingName);
+        TextView textViewAddress = listViewItem.findViewById(R.id.textViewListingAddress);
+
+        Listing listing = listingList.get(position);
+
+        textViewName.setText(listing.getListingName());
+        textViewAddress.setText(listing.getListingAddress());
+
+        return listViewItem;
+    }
+}
