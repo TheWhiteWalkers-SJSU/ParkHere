dbafe86a8e3881fbcd2fe66f84afec3da8bbf4b3
pearlor <pearlor@Yahoo.com>
2017-10-29 17:56:07 -0700

    Inbox and ViewRequest
    To Dos
    **From homepage to inbox (and back button from inbox to homepage
    
    **Filter messages (later iteration)
    
    **Set up data structure for messages
    *requestType
    *subject
    *sender
    *message
    *listingID
    hasBeenRead
    
    **Get message from database
    
    **styling

---
+++ app/src/main/java/com/thewhitewalkers/parkhere/Inbox.java
@@ -0,0 +1,62 @@
+package com.thewhitewalkers.parkhere;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Inbox extends AppCompatActivity {
+
+    private Button requestButton;
+    private TextView inboxTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inbox);
+
+        inboxTitle = findViewById(R.id.inboxTitle);
+        requestButton = findViewById(R.id.inboxRequest);
+
+
+
+        String[] myStringArray = new String[6];
+        myStringArray[0] = "Booking Success for: \n1 Washington Square\non 10/27 - 10/30\nfrom 1:30pm - 2:45 pm";
+        myStringArray[1] = "Booking Pending for: \n1 Washington Square\non 10/27 - 10/30\nfrom 1:30pm - 2:45 pm";
+        myStringArray[2] = "Booking Denied! \n1 Washington Square\non 10/1 - 10/30\nfrom 1:30pm - 2:45 pm";
+        myStringArray[3] = "Booking Denied! \n1 Washington Square\non 10/1 - 10/30\nfrom 1:30pm - 2:45 pm";
+        myStringArray[4] = "Review Request for Your Space on: \n1 Washington Square\non 11/1 - 11/30\nfrom 10:30am - 2:45 pm";
+        myStringArray[5] = "Review Request for Your Space on: \n1 Washington Square\non 12/1 - 12/30\nfrom 10:30am - 2:45 pm";
+        int requestNum = myStringArray.length; //get request number
+        if(myStringArray.length == 1){
+            myStringArray[0] = "No messages";
+            requestNum = 0;
+        }
+
+
+        requestButton.setText("Requests (" + requestNum + ")");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  R.layout.inbox_item, myStringArray);
+
+        ListView listView = (ListView) findViewById(R.id.inboxListView);
+        listView.setAdapter(adapter);
+        // Create a message handling object as an anonymous class.
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                Toast.makeText(Inbox.this,
+                        "Opening message...", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Inbox.this, ViewRequest.class));
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/thewhitewalkers/parkhere/ViewRequest.java
@@ -0,0 +1,78 @@
+package com.thewhitewalkers.parkhere;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ViewRequest extends AppCompatActivity {
+
+    private Button backToInboxButton;
+    private TextView subjectLine;
+    private TextView senderLine;
+    private TextView messageText;
+    private Button viewListingButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_request);
+
+        backToInboxButton = findViewById(R.id.backToInbox);
+        subjectLine = findViewById(R.id.subjectLine);
+        senderLine = findViewById(R.id.senderLine);
+        messageText = findViewById(R.id.message);
+        viewListingButton = findViewById(R.id.viewListing);
+
+        /*
+            0 - Owner Action Required (from renter user)
+            1 - Booking Pending (from system)
+            2 - Booking Accepted (from system)
+            3 - Booking Denied (from system)
+         */
+        int requestType = 0; //get the request type
+        if(requestType == 0){ //Owner needs to accept/ reject booking
+
+        }
+        else if(requestType == 1){
+
+        }
+        else if(requestType == 2){
+
+        }
+        else if(requestType == 3){
+
+        }
+        else{
+            Toast.makeText(ViewRequest.this,
+                    "ERROR...", Toast.LENGTH_SHORT).show();
+        }
+
+        String subject = "Booking Pending";
+        String sender = "From: Pearl";
+        String message = "Your booking request is still pending for 1 Washington Sq.";
+
+        backToInboxButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //redirect back to inbox
+                startActivity(new Intent(ViewRequest.this, Inbox.class));
+            }
+        });
+
+        subjectLine.setText(subject);
+        senderLine.setText(sender);
+        messageText.setText(message);
+
+
+        viewListingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //redirect to listing
+                Toast.makeText(ViewRequest.this,
+                        "Redirect to Listing...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
