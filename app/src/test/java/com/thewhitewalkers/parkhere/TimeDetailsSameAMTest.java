e12ad633dc212c5ab7e19742c7eb16250a2d4476
pearlor <pearlor@Yahoo.com>
2017-11-08 23:49:44 -0800

    Testcases for TimeDetails
    Started it off by writing down all the methods are neccessary for testing. Then, the made the TestCase classes with different inputs for the testcase. One testcase is for normal formating (meaning the user inputs stuff correctly). The other testcase checks to see if the user passed in non-numbers, then can the formatter check?

---
+++ app/src/test/java/com/thewhitewalkers/parkhere/TimeDetailsSameAMTest.java
@@ -0,0 +1,83 @@
+package com.thewhitewalkers.parkhere;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Pearl on 11/8/2017.
+ */
+
+public class TimeDetailsSameAMTest extends TestCase {
+    public TimeDetailsSameAMTest(){} //default constructor
+    TimeDetails timeDetails1;
+    double delta = 0.01;
+    @Before
+    protected void setUp(){
+        String startingDate  = "11/8/2017";
+        String endingDate = "11/11/2017";
+        String startingTime = "10:00";
+        String endingTime = "1:30";
+        boolean s_IsAm = true;
+        timeDetails1 = new TimeDetails(startingDate, endingDate, startingTime, s_IsAm, endingTime, !s_IsAm);
+    }
+
+    @Test
+    public void testGetHours() throws Exception{
+        //Expected 3.5 hours * 3 days = 10.5
+        double expectedNumOfHours = 3.5;
+        double actualNumOfHours = timeDetails1.getHours();
+        assertEquals(expectedNumOfHours, actualNumOfHours, delta);
+    }
+
+    @Test
+    public void testGetDays() throws Exception{
+        //Expected 3 days
+        double expectedNumOfDays = 4.0;
+        double actualNumOfDays = timeDetails1.getDays();
+        assertEquals(expectedNumOfDays, actualNumOfDays, delta);
+    }
+
+    @Test
+    public void testGetTotalHours() throws Exception{
+        //Expected 3.5 hours * 4 days = 14.0
+        double expectedTotalNumOfHours = 14.0;
+        double actualTotalNumOfHours = timeDetails1.getTotalHours();
+        assertEquals(expectedTotalNumOfHours, actualTotalNumOfHours, delta);
+    }
+
+    @Test
+    public void testSetPrice() throws Exception{
+        //Expected 3.5 hours * 4 days = 14.0
+        double pricePerHour1 = 1.50;
+        double pricePerHour2 = 10.23;
+        double pricePerhour3 = 17;
+
+        //assertEquals(expectedTotalNumOfHours, actualTotalNumOfHours, delta);
+    }
+
+    @Test
+    public void testChecKDateFormat() throws Exception{
+        //Expected 3.5 hours * 4 days = 14.0
+        double expectedTotalNumOfHours = 14.0;
+        double actualTotalNumOfHours = timeDetails1.getTotalHours();
+        assertEquals(expectedTotalNumOfHours, actualTotalNumOfHours, delta);
+    }
+
+    @Test
+    public void testCheckTimeFormat() throws Exception{
+        //Expected 3.5 hours * 4 days = 14.0
+        double expectedTotalNumOfHours = 14.0;
+        double actualTotalNumOfHours = timeDetails1.getTotalHours();
+        assertEquals(expectedTotalNumOfHours, actualTotalNumOfHours, delta);
+    }
+
+    @Test
+    public void testCheckDateValid() throws Exception{
+        //Expected 3.5 hours * 4 days = 14.0
+        double expectedTotalNumOfHours = 14.0;
+        double actualTotalNumOfHours = timeDetails1.getTotalHours();
+        assertEquals(expectedTotalNumOfHours, actualTotalNumOfHours, delta);
+    }
+}
---
+++ app/src/test/java/com/thewhitewalkers/parkhere/TimeDetailsWrongStringFormat.java
@@ -0,0 +1,8 @@
+package com.thewhitewalkers.parkhere;
+
+/**
+ * Created by Pearl on 11/8/2017.
+ */
+
+public class TimeDetailsWrongStringFormat {
+}
