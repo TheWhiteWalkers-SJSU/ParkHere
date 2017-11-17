c9a97a722f7d3a12780965de947997e0e026652c
Romsin Khoshabian <romsin.khoshabian@sjsu.edu>
2017-11-17 15:31:20 -0800

    update listing page

---
+++ app/src/main/java/com/thewhitewalkers/parkhere/UpdateListingActivity.java
@@ -0,0 +1,13 @@
+package com.thewhitewalkers.parkhere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateListingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_listing);
+    }
+}
