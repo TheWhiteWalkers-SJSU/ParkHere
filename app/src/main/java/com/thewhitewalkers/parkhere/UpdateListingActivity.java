2945582ff685bf62aa28ee968011a8dc3c9c44bd
Romsin Khoshabian <romsin.khoshabian@sjsu.edu>
2017-11-20 19:53:08 -0800

    Complete Update Listing Activity

--- app/src/main/java/com/thewhitewalkers/parkhere/UpdateListingActivity.java
+++ app/src/main/java/com/thewhitewalkers/parkhere/UpdateListingActivity.java
@@ -41,6 +41,13 @@ public class UpdateListingActivity extends AppCompatActivity {
 
         // get the TextField in the layout and map it to TextField object
         editTextListingName = findViewById(R.id.editTextListingName);
+        editTextListingAddress = findViewById(R.id.editTextListingAddress);
+        editTextListingDescription = findViewById(R.id.editTextListingDescription);
+        editTextListingPrice = findViewById(R.id.editTextListingPrice);
+        editTextListingStartDate = findViewById(R.id.editTextListingStartDate);
+        editTextListingEndDate = findViewById(R.id.editTextListingEndDate);
+        editTextListingStartTime = findViewById(R.id.editTextListingStartTime);
+        editTextListingEndTime = findViewById(R.id.editTextListingEndTime);
 
         buttonUpdateListing = findViewById(R.id.buttonUpdateListing);
         buttonUpdateListing.setOnClickListener(new View.OnClickListener() {
@@ -57,10 +64,38 @@ public class UpdateListingActivity extends AppCompatActivity {
 
         // Fetch the text entered by the user and set it to a String
         String newListingName = editTextListingName.getText().toString().trim();
+        String newListingAddress = editTextListingAddress.getText().toString().trim();
+        String newListingDescription = editTextListingDescription.getText().toString().trim();
+        String newListingPrice = editTextListingPrice.getText().toString().trim();
+        String newListingStartDate = editTextListingStartDate.getText().toString().trim();
+        String newListingEndDate = editTextListingEndDate.getText().toString().trim();
+        String newListingStartTime = editTextListingStartTime.getText().toString().trim();
+        String newListingEndtime = editTextListingEndTime.getText().toString().trim();
 
         // if the value is not empty add it to the update hashmap
         if(!newListingName.isEmpty()) {
             listingUpdate.put("listingName", newListingName);
+        }
+        if(!newListingAddress.isEmpty()) {
+            listingUpdate.put("listingAddress", newListingAddress);
+        }
+        if(!newListingDescription.isEmpty()) {
+            listingUpdate.put("listingDescription", newListingDescription);
+        }
+        if(!newListingPrice.isEmpty()) {
+            listingUpdate.put("listingPrice", newListingPrice);
+        }
+        if(!newListingStartDate.isEmpty()) {
+            listingUpdate.put("listingStartDate", newListingStartDate);
+        }
+        if(!newListingEndDate.isEmpty()) {
+            listingUpdate.put("listingEndDate", newListingEndDate);
+        }
+        if(!newListingStartDate.isEmpty()) {
+            listingUpdate.put("listingStartTime", newListingStartTime);
+        }
+        if(!newListingEndtime.isEmpty()) {
+            listingUpdate.put("listingEndTime", newListingEndtime);
         }
 
         // update the current listing with the values in the updateListing hashmap
