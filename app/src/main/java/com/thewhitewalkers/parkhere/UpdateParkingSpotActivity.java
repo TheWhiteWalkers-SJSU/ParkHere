dea45db2547caa80dde8967eb9e7233676ee65a0
rmsnkhshbn <romsin.khoshabian@sjsu.edu>
2017-11-30 22:46:02 -0800

    ViewAndUpdateParkingSpots

---
+++ app/src/main/java/com/thewhitewalkers/parkhere/UpdateParkingSpotActivity.java
@@ -0,0 +1,89 @@
+package com.thewhitewalkers.parkhere;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UpdateParkingSpotActivity extends AppCompatActivity {
+
+    private EditText editTextParkingSpotName;
+    private EditText editTextParkingSpotAddress;
+    private EditText editTextParkingSpotDescription;
+    private Button buttonUpdateParkingSpot;
+    private Button buttonBackToParkingSpot;
+
+    private ParkingSpot currentParkingSpot;
+    final DatabaseReference ParkingSpotDatabase = FirebaseDatabase.getInstance().getReference("ParkingSpots");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_parking_spot);
+
+        currentParkingSpot = (ParkingSpot) getIntent().getSerializableExtra("ParkingSpot");
+
+        editTextParkingSpotName = findViewById(R.id.editTextParkingSpotName);
+        editTextParkingSpotAddress = findViewById(R.id.editTextParkingSpotAddress);
+        editTextParkingSpotDescription = findViewById(R.id.editTextParkingSpotDescription);
+
+        buttonUpdateParkingSpot = findViewById(R.id.buttonUpdateParkingSpot);
+        buttonUpdateParkingSpot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            updateParkingSpotFields();
+            }
+        });
+
+        buttonBackToParkingSpot= findViewById(R.id.buttonBackToParkingSpot);
+        buttonBackToParkingSpot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent viewParkingSpotIntent = new Intent(getApplicationContext(), ViewParkingSpotActivity.class);
+                viewParkingSpotIntent.putExtra("ParkingSpot", currentParkingSpot);
+                startActivity(viewParkingSpotIntent);
+            }
+        });
+
+    public void updateParkingSpotFields() {
+        Map<String, Object> ParkingSpotUpdate = new HashMap<>();
+
+        // Fetch the text entered by the user and set it to a String
+        String newParkingSpotName = editTextParkingSpotName.getText().toString().trim();
+        String newParkingSpotAddress = editTextParkingSpotAddress.getText().toString().trim();
+        String newParkingSpotDescription = editTextParkingSpotDescription.getText().toString().trim();
+
+        // if the value is not empty add it to the update hashmap
+        if(!newParkingSpotName.isEmpty()) {
+            ParkingSpotUpdate.put("ParkingSpotName", newParkingSpotName);
+        }
+        if(!newParkingSpotAddress.isEmpty()) {
+            ParkingSpotUpdate.put("ParkingSpotAddress", newParkingSpotAddress);
+        }
+        if(!newParkingSpotDescription.isEmpty()) {
+            ParkingSpotUpdate.put("ParkingSpotDescription", newParkingSpotDescription);
+        }
+
+        ParkingSpotDatabase.child(currentParkingSpot.getParkingSpotId()).updateChildren(ParkingSpotUpdate)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(UpdateParkingSpotActivity.this, "Updated Parking Spot...", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), HomepageActivity.class));
+                    }
+                });
+    }
+}
+
---
+++ app/src/main/java/com/thewhitewalkers/parkhere/ViewParkingSpotActivity.java
@@ -0,0 +1,99 @@
+package com.thewhitewalkers.parkhere;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ViewParkingSpotActivity extends AppCompatActivity {
+
+    DatabaseReference ParkingSpotDatabase = FirebaseDatabase.getInstance().getReference("parkingSpots");
+    DatabaseReference requestDatabase = FirebaseDatabase.getInstance().getReference("requests");
+    FirebaseAuth firebaseAuth;
+
+    private TextView ParkingSpotNameText;
+    private TextView ParkingSpotAddressText;
+    private TextView ParkingSpotDescriptionText;
+    private TextView ParkingSpotOwnerText;
+    private Button updateButton;
+    private Button deleteButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_parking_spot);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        Intent ParkingSpotIntent = getIntent();
+        final ParkingSpot thisParkingSpot = (ParkingSpot) ParkingSpotIntent.getSerializableExtra("ParkingSpot");
+
+        String ParkingSpotName = thisParkingSpot.getParkingSpotName();
+        String ParkingSpotOwner = thisParkingSpot.getOwnerEmail();
+
+        if(ParkingSpotOwner == null) ParkingSpotOwner = thisParkingSpot.getOwnerId();
+        String ParkingSpotAddress = thisParkingSpot.getParkingSpotAddress();
+        String ParkingSpotDescription = thisParkingSpot.getParkingSpotDescription();
+        String ParkingSpotStart = thisParkingSpot.getStartTime();
+        String ParkingSpotEnd = thisParkingSpot.getEndTime();
+
+        ParkingSpotNameText = findViewById(R.id.ParkingSpotNameText);
+        ParkingSpotOwnerText = findViewById(R.id.ParkingSpotOwnerText);
+        ParkingSpotAddressText = findViewById(R.id.ParkingSpotAddressText);
+        ParkingSpotDescriptionText = findViewById(R.id.ParkingSpotDescriptionText);
+
+        deleteButton = findViewById(R.id.deleteButton);
+        updateButton = findViewById(R.id.updateButton);
+
+        ParkingSpotNameText.setText(ParkingSpotName);
+        ParkingSpotOwnerText.setText(ParkingSpotOwner);
+        ParkingSpotAddressText.setText(ParkingSpotAddress);
+        ParkingSpotDescriptionText.setText(ParkingSpotDescription);
+
+        if(thisParkingSpot.getOwnerId() != null) {
+            if (!thisParkingSpot.getOwnerEmail().equals(user.getEmail())) {
+                updateButton.setVisibility(View.GONE);
+            }
+        }
+
+        if(thisParkingSpot.getOwnerId() != null) {
+            if (thisParkingSpot.getOwnerId().equals(user.getEmail()) || thisParkingSpot.getOwnerId().equals(user.getUid())) {
+                deleteButton.setVisibility(View.VISIBLE);
+                updateButton.setVisibility(View.VISIBLE);
+            } else {
+                deleteButton.setVisibility(View.GONE);
+                updateButton.setVisibility(View.GONE);
+            }
+        }
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Intent updateParkingSpotIntent = new Intent(getApplicationContext(), UpdateParkingSpotActivity.class);
+            updateParkingSpotIntent.putExtra("ParkingSpot", thisParkingSpot);
+            startActivity(updateParkingSpotIntent);
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), HomepageActivity.class));
+                Toast.makeText(ViewParkingSpotActivity.this, "Deleted ParkingSpot...", Toast.LENGTH_SHORT).show();
+                ParkingSpotDatabase.child(thisParkingSpot.getParkingSpotId()).removeValue();
+            }
+        });
+
+
+    }
+}
